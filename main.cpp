b0e923ed73e388c1e47f289873681b50a9b06d50
Timothy Kim <tyk1@williams.edu>
2023-06-26 15:47:22 -0400

    new implement

--- main.cpp
+++ main.cpp
@@ -7,19 +7,24 @@ int main(int argc, char* argv[]){
 #define LENGTH atoi(argv[1])
 #undef NUM_HASH_FXNS
 #define NUM_HASH_FXNS atoi(argv[2])
-  
+
   hash<int> int_hash;
   BloomFilter<int> c(LENGTH, NUM_HASH_FXNS, int_hash);
 
   c.insert(300);
   c.insert(551);
 
-  cout << c.query(330) + "\n"; //1
-  cout << c.query(551) + "\n"; //1
-  cout << c.query(7) + "\n"; //0
-  cout << "Total number of bits allocated: " + c.total_bits_allocated();
+  cout << c.query(330);
+  cout <<  "\n"; //1
+  cout << c.query(551);
+  cout << "\n"; //1
+  cout << c.query(7);
+  cout << "\n"; //0
+  cout << "Total number of bits allocated: ";
+  cout << c.total_bits_allocated();
   cout << "\n";
-  cout << "Bits user asked for: " + c.bits_user_asked_for();
+  cout << "Bits user asked for: ";
+  cout << c.bits_user_asked_for();
   cout << "\n";
 
   return 0;
