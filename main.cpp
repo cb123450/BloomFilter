6376d8816770b8b8eb598ca3ff8efade32370406
Christopher Johnathan Brown <cjb4@williams.edu>
2023-06-29 15:51:43 -0400

    errors with producer-consumer

--- main.cpp
+++ main.cpp
@@ -6,18 +6,104 @@
 #include <pthread.h>
 #include <cstring>
 #include <assert.h>
-pthread_spinlock_t spinlock;
+#include <vector>
+#define TYPE string
+#define MAX 1000
 
+
+/*
+ * Buffer implementation for producer-consumer tests
+ */
+tuple<string, TYPE> buff[MAX];
+int put_pointer = 0;
+int get_pointer = 0;
+int count = 0;
+
+void put(tuple<string, TYPE> val) {
+  buff[put_pointer] = val;
+  put_pointer = (put_pointer + 1) % MAX;
+  count += 1;
+}
+
+tuple<string, TYPE> get(){
+  TYPE ret = buff[get_pointer];
+  get_pointer = (get_pointer + 1) % MAX;
+  count -= 1;
+  return ret;
+}
+
+pthread_cond_t empty, fill;
+pthread_mutex_t mutex;
+
+/*
+ * producer threads
+ */
+void* producer(void* param){
+  int num_tasks = param.n_t;
+  
+  srand(time(0));
+  for (int i = 0; i < num_tasks; i++) {
+    pthread_mutex_lock(&mutex);
+    while (count == MAX){
+      pthread_cond_wait(&empty, &mutex);
+    }
+    string RW = "W";
+    if (rand() % 2) {
+      RW = "R";
+    }
+    auto tuple = make_tuple(RW, (rand() % 5000) + 1);
+    put(tuple);
+    pthread_cond_signal(&fill);
+    pthread_mutex_unlock(&mutex);
+  }
+}
+
+/*
+ * consumer threads
+ */
+string action;
+TYPE item;
+void* consumer(void* param){
+  
+  thread_data<K>* td = (thread_data<K> *) param;
+  int num_tasks = param.n_t;
+
+  for (i = 0; i < num_tasks; i++){
+    pthread_mutex_lock(&mutex);
+    
+    while(count == 0){
+      pthread_cond_wait(&fill, &mutex);
+
+      std::tie (action, item) = get();
+
+      //Insert item into BloomFilter
+      if (action.compare("R") == 0) {
+	bf->query(item);
+      } else if (action.compare("W") == 0) {
+	bf->insert(item);
+      }
+
+      pthread_cond_signal(&empty);
+      pthread_mutex_unlock(&mutex);
+    }
+  }
+}
+
+/*
+ * struc to hold data about our bloomfilter to pass it to 'run_t'
+ */
 template<typename T>
 struct thread_data{
   BloomFilter<T>* bf;
-  queue<tuple<string, T>>* q;
+  int n_t;
 };
+
+
 
 /*
  * K is the type of 'item'
  * This is the type of the object stored in the BloomFilter and the item stored in the queue
- */
+ 
 template<typename K>
 void *run_t(void *param){
 
@@ -63,130 +149,37 @@ void *run_t(void *param){
   return NULL;
 }
 
+*/
 int main(int argc, char* argv[]){
 #undef LENGTH
 #define LENGTH atoi(argv[1])
 #undef NUM_HASH_FXNS
 #define NUM_HASH_FXNS atoi(argv[2])
-#define NUM_THREADS atoi(argv[3])
+#define NUM_CONSUMERS atoi(argv[3])
 #define NUM_TASKS atoi(argv[4])
 
-  int rc = pthread_spin_init(&spinlock, PTHREAD_PROCESS_PRIVATE);
+  int rc = pthread_mutex_init(&mutex, PTHREAD_PROCESS_PRIVATE);
   assert(rc == 0);
-  hash<int> _hash;
-  BloomFilter<int> c(LENGTH, NUM_HASH_FXNS, _hash);
-  queue<tuple<string, int>> q;  
-  thread_data<int> td = {&c, &q};
+  hash<TYPE> _hash;
+  BloomFilter<TYPE> c(LENGTH, NUM_HASH_FXNS, _hash);
+  queue<tuple<string, TYPE>> q;  
+  thread_data<int> td = {&c, NUM_TASKS};
 
-  srand(time(0));
-  for (int i = 0; i < NUM_TASKS; i++) {
-    string RW = "W";
-    if (rand() % 2) {
-      RW = "R";
-    }
-    auto tuple = make_tuple(RW, (rand() % 5000) + 1);
-    q.push(tuple);
-  }
   
-  /*  
-  auto a = make_tuple("W", "bat");
-  auto b = make_tuple("W", "car");
-  auto m = make_tuple("W", "cat");
-  auto d = make_tuple("W", "tree");
-  auto e = make_tuple("W", "bush");
-  auto f = make_tuple("W", "brick");
-  auto g = make_tuple("R", "bat");
-  auto h = make_tuple("R", "car");
-  auto i = make_tuple("R", "cat");
-  auto j = make_tuple("R", "boat");
-  auto k = make_tuple("R", "block");
-  auto l = make_tuple("R", "house");
+  pthread_t * consumers = new pthread_t[NUM_CONSUMERS];
+  for (int j = 0; j < NUM_CONSUMERS; j++) {
+    pthread_create(&pthreads[j], NULL, consumer, &td);
+  }
 
-  q.push(a);
-  q.push(b);
-  q.push(m);
-  q.push(d);
-  q.push(e);
-  q.push(f);
-  q.push(g);
-  q.push(h);
-  q.push(i);
-  q.push(j);
-  q.push(k);
-  q.push(l);
-  */
+  pthread_t* producer;
+  pthread_create(&producer, NULL, producer, &td);
   
-  pthread_t * pthreads = new pthread_t[NUM_THREADS];
-  for (int j = 0; j < NUM_THREADS; j++) {
-    pthread_create(&pthreads[j], NULL, run_t<int>, &td);
-  }
-  for (int k = 0; k < NUM_THREADS; k++) {
+  for (int k = 0; k < NUM_CONSUMERS; k++) {
     pthread_join(pthreads[k], NULL);
   }
+
+  pthread_join(producer, NULL);
   
   return 0;
-
-  /* INTS work
-  hash<int> _hash;
-  BloomFilter<int> c(LENGTH, NUM_HASH_FXNS, _hash);
-
-  c.insert(5);
-  c.insert(400);
-
-  cout << _hash(5);
-  cout << "\n";
-  cout << c.query(5); //1
-  cout << "\n";		  
-  cout << c.query(400); //1
-  cout << "\n";		  
-  cout << c.query(10); //0
-  cout << "\n";		  
-  */
-
-  /* STRING TESTS
-  hash<string> _hash;
-  BloomFilter<string> c(LENGTH, NUM_HASH_FXNS, _hash);
-
-  c.insert("bat");
-  c.insert("car");
-
-  cout << _hash("bat");
-  cout << "\n";
-  cout << c.query("bat"); //1
-  cout << "\n";
-  cout << c.query("car"); //1
-  cout << "\n";
-  cout << c.query("tree"); //0
-  cout << "\n";
-  
-  cout << "Total number of bits allocated: ";
-  cout <<  c.total_bits_allocated();
-  cout << "\n";
-  cout << "Bits user asked for: ";
-  cout << c.bits_user_asked_for();
-  cout << "\n";
-  */
-
-  /* CountingBloomFilter Tests
-  c.insert(3);
-  c.insert(3);
-  c.insert(3);
-  c.insert(7);
-
-  cout << c.query(3,2); // 1
-  cout << "\n";
-  cout << c.query(3,3); // 1
-  cout << "\n";
-  cout << c.query(3,4); // 0
-  cout << "\n";
-  cout << c.query(7,1); // 1
-  cout << "\n";
-  cout << c.query(5,1); // 0
-  cout << "\n";
-
-  c.deleteObj(3);
-  cout << c.query(3,3); // 0
-  cout << "\n";
-  */
   
 }
